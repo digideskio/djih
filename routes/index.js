6d1879056058c3c4ed9e6cf8227e881f11232fdd
Daniel Jih <djih@stanford.edu>
2014-07-30 23:02:35 -0700

    cleaned up app

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+// var pg = require('pg');
+// var params = { host: 'ec2-54-225-101-119.compute-1.amazonaws.com', port: '5432', user: 'fijgchjkjpensc', password: 'zdGW3MUStdxwzq3nMGi5LQdT-Z', database: 'd26hna95h4r3en', ssl: true };
+
+
+exports.view = function(req, res){
+    var data = [];
+    res.render('index', data);
+};
