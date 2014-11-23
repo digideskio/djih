57f977b67e109be5692c9841f97c9a03d0ffae37
Daniel Jih <djih@stanford.edu>
2014-11-22 22:50:32 -0800

    cleaning up

--- routes/food_agency.js
+++ routes/food_agency.js
@@ -1,4 +1,7 @@
 exports.view = function(req, res){
+    var date = new Date();
+    var dateString = (date.getMonth()+1) + "/" + date.getDate();
     var data = [];
+    data['date'] = dateString;
     res.render('food_agency', data);
 };
