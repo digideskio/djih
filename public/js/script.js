67e12cf56536647959d4526088f5c5424b52701b
Daniel Jih <djih@stanford.edu>
2015-12-01 09:55:23 -0800

    toggle sidebar header using css instead of jquery

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,17 @@
 $(document).ready(function() {
   $('#close-sidebar').click(function(e) {
     e.preventDefault();
-    $('#sidebar').hide(400);
-    $('#header').delay(400).slideDown(400);
+    $('.body-wrapper').toggleClass('toggled');
+    // $('#sidebar').hide(400);
+    // $('#header').delay(400).slideDown(400);
     return false;
   });
 
   $('#toggle-sidebar').click(function(e) {
     e.preventDefault();
-    $('#header').slideUp(400);
-    $('#sidebar').delay(400).show(400);
+    $('.body-wrapper').toggleClass('toggled');
+    // $('#header').slideUp(400);
+    // $('#sidebar').delay(400).show(400);
     return false;
   });
 
