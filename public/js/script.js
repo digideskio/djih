ed2abc641cb651d936630405997311d3afa26873
Daniel Jih <djih@stanford.edu>
2015-12-01 09:25:22 -0800

    fix headers

--- public/js/script.js
+++ public/js/script.js
@@ -3,21 +3,23 @@ $(document).ready(function() {
     e.preventDefault();
     $('#sidebar').hide(400);
     $('#header').delay(400).slideDown(400);
+    return false;
   });
 
   $('#toggle-sidebar').click(function(e) {
     e.preventDefault();
     $('#header').slideUp(400);
     $('#sidebar').delay(400).show(400);
+    return false;
   });
 
-  /*
+  var galleryPosition = $('.gallery').position().top;
+
   $('#content').scroll(function() {
-    if ($('#content').scrollTop() > 50) {
-      $("#header-background").addClass('scrolling');
+    if ($('#content').scrollTop() > galleryPosition) {
+      $("#header-background").addClass('visible');
     } else {
-      $("#header-background").removeClass('scrolling');
+      $("#header-background").removeClass('visible');
     }
   });
-  */
 });
