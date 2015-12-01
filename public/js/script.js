bbc70a7ac690757267fd0f52949c2b09a1830690
Daniel Jih <djih@stanford.edu>
2015-12-01 08:58:24 -0800

    fixed jquery sliding header direction

--- public/js/script.js
+++ public/js/script.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
   $('#close-sidebar').click(function(e) {
     e.preventDefault();
     $('#sidebar').hide(400);
-    $('#header').delay(400).show(400);
+    $('#header').delay(400).slideDown(400);
   });
 
   $('#toggle-sidebar').click(function(e) {
     e.preventDefault();
-    $('#header').hide(400);
+    $('#header').slideUp(400);
     $('#sidebar').delay(400).show(400);
   });
 
