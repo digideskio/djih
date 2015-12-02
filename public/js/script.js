df74679ded05ea8c7bf44a4ffe8f0f206e8f746d
Daniel Jih <djih@stanford.edu>
2015-12-01 23:58:41 -0800

    added masthead image background

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,11 @@ $(document).ready(function() {
   $('#close-sidebar').click(function(e) {
     e.preventDefault();
     $('.body-wrapper').toggleClass('toggled');
-    // $('#sidebar').hide(400);
-    // $('#header').delay(400).slideDown(400);
-    return false;
   });
 
   $('#toggle-sidebar').click(function(e) {
     e.preventDefault();
     $('.body-wrapper').toggleClass('toggled');
-    // $('#header').slideUp(400);
-    // $('#sidebar').delay(400).show(400);
-    return false;
   });
 
   var galleryPosition = $('.gallery').position().top;
