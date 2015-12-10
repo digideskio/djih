66edbb69702b58f9149b6587c07bfc8d3144bf2a
Daniel Jih <djih@stanford.edu>
2015-12-09 23:38:16 -0800

    renabled gallery popup for nonmobile views

--- routes/album.js
+++ routes/album.js
@@ -6,7 +6,7 @@ format.extend(String.prototype);
 
 var album_query = 'SELECT a.location, a.date, a.category, p.filename, p.width, p.height FROM albums AS a INNER JOIN photos AS p ON a.cover_photo_id = p.id WHERE a.id = {0}';
 // var photo_query = 'SELECT p.filename, p.title, p.location, p.camera, p.focal_length, p.aperture, p.shutter_speed, p.iso, p.date_taken, p.width, p.height FROM album_photos AS a INNER JOIN photos AS p ON a.photo_id = p.id WHERE a.album_id = {0} ORDER BY p.date_taken, p.filename ASC;';
-var photo_query = 'SELECT p.filename, p.title, p.location, p.camera, p.focal_length, p.aperture, p.shutter_speed, p.iso, p.date_taken, p.width, p.height FROM album_photos AS a INNER JOIN photos AS p ON a.photo_id = p.id WHERE a.album_id = {0} ORDER BY RANDOM();';
+var photo_query = 'SELECT p.id, p.filename, p.title, p.location, p.camera, p.focal_length, p.aperture, p.shutter_speed, p.iso, p.date_taken, p.width, p.height FROM album_photos AS a INNER JOIN photos AS p ON a.photo_id = p.id WHERE a.album_id = {0} ORDER BY RANDOM();';
 
 var albums = [1, 2, 3];
 var months = [
