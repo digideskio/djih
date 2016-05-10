f1ac34a4a2fd4df22a8c594cbdf8e0971aabdf53
Daniel Jih <djih@stanford.edu>
2016-05-09 21:51:04 -0700

    clean up geo.js

--- routes/geo.js
+++ routes/geo.js
@@ -74,39 +74,4 @@ exports.view = function(req, res){
             })
         });
     });
-
-    // pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    //     client.query(album_query, function(err, result) {
-    //         done();
-    //         if (err) {
-    //             console.err(err); response.send("Error " + err);
-    //         } else {
-    //             var albums = result.rows;
-    //             for (var i = 0; i < result.rowCount; i++) {
-    //                 var album = albums[i];
-
-    //                 if (utils.isValidAlbum(album.id)) {
-    //                     album.show_link = true;
-    //                 }
-
-    //                 // strip off country from location string
-    //                 var city = album.location;
-    //                 var comma = city.indexOf(',');
-    //                 if (comma >= 0) {
-    //                     city = city.substring(0, comma);
-    //                 }
-    //                 album.city = city;
-
-    //                 // format date to Month Year
-    //                 var date = new Date(album.date);
-    //                 date = months[date.getMonth()] + ' ' + date.getFullYear();
-    //                 album.date = date;
-    //             }
-
-    //             data.albums = albums;
-    //             res.render('version2', data);
-    //         }
-    //     })
-    // });
-    res.render('geo', data);
 };
